88a911390a704d82b1923b1d9e1b0db6edd320f6
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-01 12:33:28 +0330

    add auth

---
+++ app/(root)/ask-question/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>Ask Question</div>
+  )
+}
+
+export default Page
--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="h1-bold">nextJs 14</h1>
-    </div>
-  );
-}
