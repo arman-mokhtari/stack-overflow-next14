3375914a814deffb5f5841ffcda0916c09a15d8d
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-05 08:16:23 +0330

    create and update user

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,8 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import { CreateUserParams, UpdateUserParams } from "./shared.types";
+import { revalidatePath } from "next/cache";
 
 export async function getUserById(params: any) {
   try {
@@ -9,6 +11,33 @@ export async function getUserById(params: any) {
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+}
+
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    connectToDatabase();
+    const { clerkId, updateData, path } = params;
+
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+    revalidatePath(path);
   } catch (error) {
     console.error(error);
     throw error;
