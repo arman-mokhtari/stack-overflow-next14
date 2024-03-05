a13ec300809a07c3fb74f41700096dba8efb7bda
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-05 15:54:26 +0330

    assign domain

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,6 +1,5 @@
 import Question from "@/components/forms/question";
 import { getUserById } from "@/lib/actions/user.action";
-// import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
