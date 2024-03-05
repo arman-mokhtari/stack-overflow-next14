0747949754b3177ed02c91cca4236edeb0c6accb
Arman Mokhtari <rmn.mokhtarii@gmail.com>
2024-03-05 15:39:19 +0330

    bug fixed

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,6 @@
 import Question from "@/components/forms/question";
 import { getUserById } from "@/lib/actions/user.action";
+// import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 const Page = async () => {
